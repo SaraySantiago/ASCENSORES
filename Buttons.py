3a65e6508541bb586459fe9028fc91509aac82ca
SaraySantiago <ssantiago18@iesrioarba.es>
2024-11-21 13:29:28 +0100

    .

---
+++ Buttons.py
@@ -0,0 +1,34 @@
+
+class boton: 
+    number = None
+    light = "off" 
+
+    def __init__(self, num) -> None:
+        self.number = num 
+        
+    def __str__(self) -> str:
+        return f"({str(self.number)+ self.light})"
+        
+class botonera:
+    pass
+
+class botoneraExterior(botonera):
+    pass
+
+class botoneraInterior(botonera):
+    pass
+
+
+
+
+
+if __name__ == "__main__":
+    
+    b = boton(4) 
+    print(b)
+  
+
+
+     
+     
+                                                                  
